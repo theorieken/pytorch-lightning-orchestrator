77366e1f2ba376cff5fb61a7daf764a75866279f
Theo Rieken <mail@theorieken.de>
2023-05-18 00:37:21 +0200

    update

--- src/Modules/simple_linear_module.py
+++ src/Modules/simple_linear_module.py
@@ -11,18 +11,52 @@ class SimpleLinearModule(BaseModule):
         # Hyperparameters
         self.learning_rate = self.params["learning_rate"]
 
-        # Simple linear model
-        self.model = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(512 * 512, 256),
+        # # Simple linear model
+        # self.model = nn.Sequential(
+        #     nn.Flatten(),
+        #     nn.Linear(512 * 512, 256),
+        #     nn.ReLU(),
+        #     nn.Linear(256, 64),
+        #     nn.ReLU(),
+        #     nn.Linear(64, 4)
+        # )
+
+        # U-Net model
+        self.downsample = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Linear(256, 64),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Linear(64, 4)
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
+        self.upsample = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 4, kernel_size=1, stride=1)
+        )
+
+    # def forward(self, x):
+    #     return self.model(x)
+
     def forward(self, x):
-        return self.model(x)
+        # Downsample path
+        x1 = self.downsample(x)
+        x2 = self.downsample(x1)
+        x3 = self.downsample(x2)
+
+        # Upsample path
+        x = self.upsample(x3)
+        x = torch.cat([x, x3], dim=1)
+        x = self.upsample(x)
+        x = torch.cat([x, x2], dim=1)
+        x = self.upsample(x)
+        x = torch.cat([x, x1], dim=1)
+        x = self.upsample(x)
+
+        return x
 
     def training_step(self, batch, batch_idx):
         x, y = batch
